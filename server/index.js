2c316741dd8495d3e66c07342855df5c66d92149
ninadthepade <ninad.thepade@gmail.com>
2020-05-12 18:51:53 +0530

    server running on localhost:5000

--- server/index.js
+++ server/index.js
@@ -6,8 +6,12 @@ const app = express()
 const server = http.createServer(app)
 const io = socketIO(server)
 
+const router = require ('./router')
+
 // Port 5000 used in local setup.
 // When running in production mode, the process.env will get a port automatically
 const PORT = process.env.PORT || 5000
+
+app.use(router)
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`))
